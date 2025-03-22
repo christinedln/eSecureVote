4134956460d96daa2a247312a305456a3c624530
sijeeyy <deleonchristinejoyce1@gmail.com>
2025-03-22 23:31:37 +0800

    remove generate_user_id

--- utils.py
+++ utils.py
@@ -1,25 +1,11 @@
 import bcrypt
-from database_service import db  # Import Firestore instance
-
+from database_service import DatabaseService
+db_service = DatabaseService()
 def hash_password(password, role):
-    """Hashes the password only for voters, keeps plain text for admins."""
     if role.lower() == "admin":
-        return password 
+        return password
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt).decode()
 
-def generate_user_id():
-    """Generates a sequential user ID (usid01, usid02, etc.)."""
-    users_ref = db.collection("Users")
-    users = users_ref.stream()
 
-    user_ids = [doc.id for doc in users if doc.id.startswith("usid")]
 
-    if not user_ids:
-        return "usid01"
-
-    user_numbers = [int(uid[4:]) for uid in user_ids if uid[4:].isdigit()]
-    
-    new_id_number = max(user_numbers) + 1 if user_numbers else 1
-
-    return f"usid{new_id_number:02d}"
