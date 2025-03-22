5742511573872b3734b882dc7db72bef8427e1b3
sijeeyy <deleonchristinejoyce1@gmail.com>
2025-03-22 23:40:25 +0800

    add vote_count

--- candidate.py
+++ candidate.py
@@ -6,47 +6,63 @@ class Candidate:
         self.__is_independent = is_independent
         self.__political_party = political_party
         self.__candidate_id = candidate_id if candidate_id else self.__generate_candidate_id()
+        self.__vote_count = 0
+
 
     def get_name(self) -> str:
         return self.__name
 
+
     def get_position(self) -> str:
         return self.__position
 
-    def set_name(self, name: str):
 
+    def set_name(self, name: str):
         self.__name = name
+
 
     def get_candidate_id(self) -> str:
         return self.__candidate_id
 
+
     def set_position(self, position: str):
         self.__position = position
 
+
     def get_is_independent(self) -> bool:
         return self.__is_independent
-    
-    def set_is_independent(self, is_independent:bool):
+   
+    def set_is_independent(self, is_independent: bool):
         self.__is_independent = is_independent
-    
+   
     def get_political_party(self) -> str:
         return self.__political_party
+
 
     def set_political_party(self, political_party: str):
         self.__political_party = political_party
 
-    def __generate_candidate_id(self) -> str:
 
+    def get_vote_count(self) -> int:
+        return self.__vote_count
+
+
+    def increment_vote_count(self):
+        self.__vote_count += 1
+
+
+    def __generate_candidate_id(self) -> str:
         db_service = DatabaseService()
         last_candidate = db_service.get_last_candidate()
 
+
         if last_candidate:
             last_id = last_candidate["candidate_id"]
-
             if last_id.startswith("caid") and last_id[4:].isdigit():
-                num = int(last_id[4:]) + 1  
+                num = int(last_id[4:]) + 1
                 new_id = f"caid{num:02d}"
+                return new_id
 
-                return new_id  
 
         return "caid01"
+
