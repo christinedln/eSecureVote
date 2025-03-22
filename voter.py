ad568c1145f506b70ebe0e996e007d267ffca4dc
sijeeyy <deleonchristinejoyce1@gmail.com>
2025-03-22 23:32:45 +0800

    add cast_vote and view_results

--- voter.py
+++ voter.py
@@ -1,31 +1,79 @@
-from database_service import add_new_user, check_registered_voter
 from user import User
+from database_service import DatabaseService
+from vote import Vote
+from register_service import VoterRegistrationService
 
 class Voter(User):
-    def __init__(self, user_id, name, email, password, province, municipality, address, face_data, has_voted=False):
-        super().__init__(user_id, name, email, password, "Voter", province, municipality, face_recog=True)
-        self.__address = address
-        self.__face_data = face_data
-        self.__has_voted = has_voted
+    _db_service = DatabaseService()
+    _register_service=VoterRegistrationService()
+
+
+    def __init__(self, user_id, name=None, email=None, password=None, province=None, municipality=None, address=None, face_recog=None, has_voted=False):
+        if name is None:  
+            user_data = self._db_service.get_user_data(user_id)
+
+
+            if not user_data or user_data.get("role") != "Voter":
+                raise ValueError("User is not a voter or does not exist.")
+           
+            name = user_data.get("name")
+            email = user_data.get("email")
+            password = user_data.get("password")
+            province = user_data.get("province")
+            municipality = user_data.get("municipality")
+            face_recog = user_data.get("face_recog", None)  
+            has_voted = user_data.get("has_voted", False)
+
+
+        super().__init__(user_id, name, email, password, "Voter", province, municipality, face_recog)
+
+
+        self.__votes = []
+
 
     def get_address(self):
         return self.__address
-    
-    def has_already_voted(self):
-        return self.__has_voted
-    
+   
     def set_address(self, address):
         self.__address = address
-    
-    def set_has_voted(self, status):
+
+
+    def get_face_recog(self) -> str:
+        return self.__face_data
+
+
+    def set_face_recog(self, face_data: str):
+        self.__face_data = face_data
+
+
+    def has_already_voted(self) -> bool:
+        return self.__has_voted
+
+
+    def set_has_voted(self, status: bool):
         self.__has_voted = status
-    
+
+
+    def get_votes(self):
+        return self.__votes  
+
+
+    def cast_vote(self, vote: Vote) -> bool:
+        for existing_vote in self.__votes:
+            if existing_vote.get_position() == vote.get_position():
+                return False  
+        self.__votes.append(vote)  
+        return True  
+   
     def register(self):
-        if check_registered_voter(self.get_email()):
-            voter_data = self.to_dict()
-            voter_data.update({"address": self.__address, "face_data": self.__face_data, "has_voted": self.__has_voted})
-            add_new_user(self.get_user_id(), voter_data)
-            print("✅ Voter registration successful!")
-            return self
-        print("❌ Registration failed: You are not a registered voter.")
-        return None
+        return self._register_service.register_voter(
+            self.get_email(), self.get_name(), self.get_password(), "Voter",
+            self.get_province(), self.get_municipality()
+        )
+   
+    def view_results(self, election_id:str):
+        results = self._db_service.get_election_result(election_id)
+        return results
+
+
+
